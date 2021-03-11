6c72da7ad67d0cc87286dc4af502d7fa670c04de
sinfonianartist <sinfonianartist@gmail.com>
2021-03-11 09:09:33 -0500

    Updated German Localization

--- Localization/deDE.lua
+++ Localization/deDE.lua
@@ -1,15 +1,15 @@
 local L = select(2, ...)[2]('deDE')
 
 -- Default tab names
-L['GUILD'] = GILDE
-L['FRIENDS'] = FREUNDE
-L['PARTY'] = PARTY
-L['RAID'] = RAID
+L['GUILD'] = 'GILDE'
+L['FRIENDS'] = 'FREUNDE'
+L['PARTY'] = 'GRUPPE'
+L['RAID'] = 'RAID'
 
 -- Column Headers
-L['LEVEL'] = STUFE
-L['DUNGEON'] = DUNGEONS
-L['CHARACTER'] = CHARAKTER
+L['LEVEL'] = 'STUFE'
+L['DUNGEON'] = 'DUNGEONS'
+L['CHARACTER'] = 'CHARAKTER'
 
 -- Subsection Headers
 L['CHARACTERS'] = 'CHARAKTERE'
@@ -17,29 +17,38 @@ L['AFFIXES'] = 'AFFIXE'
 
 -- Character Labels
 L['CURRENT_KEY'] = 'AKTUELL'
-L['WEEKLY_BEST'] = 'WÖCH. BESTES'
+L['WEEKLY_BEST'] = 'WÖCH. BESTER'
 
 L['CHARACTER_DUNGEON_NOT_RAN'] = 'Kein Mythisch+ abgeschlossen'
 L['CHARACTER_KEY_NOT_FOUND'] = 'Kein Schlüssel gefunden'
 
 
 -- Dropdown menu selections
-L['Whisper'] = FLÜSTERN
-L['INVITE'] = EINLADEN
-L['SUGGEST_INVITE'] = EINLADUNG EMPFEHLEN
-L['REQUEST_INVITE'] = EINLADUNG NACHFRAGEN
-L['CANCEL'] = ABBRECHEN
+L['REPORT_TO'] = 'Melden an'
+L['Whisper'] = 'Anflüstern'
+L['INVITE'] = 'Einladen'
+L['SUGGEST_INVITE'] = "Einladung vorschlagen"
+L['REQUEST_INVITE'] = "Einladung anfragen"
+L['CANCEL'] = 'ABBRECHEN'
+L['NEW_LIST_DESCRIPTION'] = 'Neuer Listenname'
+L['CREATE_NEW_LIST'] = 'Neue Liste erstellen'
+L['REMOVE_UNIT_FROM_LIST'] = 'Von Liste entfernen'
+L['ADD_TO_LIST'] = 'Zur Liste hinzufügen'
+L['ADD_REMOVE_LIST'] = 'Liste hinzufügen oder löschen'
+L['DELETE_LIST'] = 'Liste löschen'
+L['LIST_ADD_HELPER_TEXT'] = 'Jemanden durch Rechtsklick in der GILDEN- oder FREUNDESLISTE hinzufügen'
+L['OKAY'] = OKAY
 
 -- Announce messages
 L['ANNOUNCE_NEW_KEY'] = 'Astral Keys: Neuer Schlüssel %s'
 L['NO_KEY'] = 'Kein Schlüssel'
-L['KEYS_RESPOND_ON_NO_KEY'] = 'Antworten auch mit keinem Schlüssel vorhanden'
+L['KEYS_RESPOND_ON_NO_KEY'] = 'Ohne eigenen Schlüssel reagieren'
 
 
 -- Search field texts
-L['FILTER_TEXT_DUNGEON'] = 'Nach Dungeon filtern'
-L['FILTER_TEXT_CHARACTER'] = 'Nach Charakternamen filtern'
+L['FILTER_TEXT_DUNGEON'] = 'Sortierung nach Dungeon'
+L['FILTER_TEXT_CHARACTER'] = 'Sortierung nach Charakternamen'
 
 -- Options
-L['!KEYS_DESC'] = 'Auf !keys antworten in den folgenden Kanälen'
-L['EXPANDED_TOOLTIP'] = 'Affix-Beschreibung im Tooltip anzeigen'
+L['!KEYS_DESC'] = 'In folgenden Chatkanälen auf !keys Befehl reagieren'
+L['EXPANDED_TOOLTIP'] = 'Zeige Affix-Beschreibungen im Tooltip'
