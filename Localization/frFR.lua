c14d173d0033dd80ad25ca63f97d15f8da88193f
Crapolion <77567752+Crapolion@users.noreply.github.com>
2023-01-21 14:23:12 +0100

    Update frFR.lua
    Hello, I offer you the full translation of the addon with updated Dragoflight dungeon names.
    
    It seems to me that there are only three variables that I did not find:
    "Current Keystone" in the tooltip,
    "Close" and "Visit Astral at" in the options.
    
    Have a good day.
    Crapolion/Arciduc - Dalaran EU

--- Localization/frFR.lua
+++ Localization/frFR.lua
@@ -1,7 +1,7 @@
 local L = select(2, ...).L('frFR')
 
 -- Default string for keystone
-L['KEYSTONE'] = 'Keystone:'
+L['KEYSTONE'] = 'clé :'
 
 -- Default tab names
 L['GUILD'] = GUILD
@@ -15,15 +15,15 @@ L['DUNGEON'] = DUNGEONS
 L['CHARACTER'] = CHARACTER
 
 -- Subsection Headers
-L['CHARACTERS'] = 'CHARACTERS'
+L['CHARACTERS'] = 'PERSONNAGES'
 L['AFFIXES'] = 'AFFIXES'
 
 -- Character Labels
-L['CURRENT_KEY'] = 'CURRENT'
-L['WEEKLY_BEST'] = 'WKLY BEST'
+L['CURRENT_KEY'] = 'ACTUELLE'
+L['WEEKLY_BEST'] = 'BEST HEBDO'
 
-L['CHARACTER_DUNGEON_NOT_RAN'] = 'No mythic+ ran'
-L['CHARACTER_KEY_NOT_FOUND'] = 'No key found'
+L['CHARACTER_DUNGEON_NOT_RAN'] = "Aucun donjon mythique+ n'a été fait"
+L['CHARACTER_KEY_NOT_FOUND'] = 'Aucune clé trouvée'
 
 
 -- Dropdown menu selections
@@ -34,18 +34,41 @@ L['REQUEST_INVITE'] = REQUEST_INVITE
 L['CANCEL'] = CANCEL
 
 -- Announce messages
-L['ANNOUNCE_NEW_KEY'] = 'Astral Keys: New key %s'
-L['NO_KEY'] = 'No key'
-L['KEYS_RESPOND_ON_NO_KEY'] = 'Respond even if you do not have a key'
+L['ANNOUNCE_NEW_KEY'] = 'Astral Keys : Nouvelle clé %s'
+L['NO_KEY'] = 'Pas de clé'
+L['KEYS_RESPOND_ON_NO_KEY'] = "Répondre même si vous n'avez pas de clé"
 
 
 -- Search field texts
-L['FILTER_TEXT_DUNGEON'] = 'Filter by dungeon'
-L['FILTER_TEXT_CHARACTER'] = 'Filter by character name'
+L['FILTER_TEXT_DUNGEON'] = 'Filtrer par donjon'
+L['FILTER_TEXT_CHARACTER'] = 'Filtrer par nom de personnage'
 
--- Options
-L['!KEYS_DESC'] = 'Respond to !keys command in the following chat channels'
-L['EXPANDED_TOOLTIP'] = 'Show affix description in the tooltip'
+
+-- General Options
+
+L['GENERAL OPTIONS'] = 'OPTIONS GÉNÉRALES'
+L['Show offline pLayers'] = 'Afficher les joueurs déconnectés'
+L['Show Minimap button'] = 'Afficher le bouton sur la Minimap'
+L['Show current key in tooltip'] = "Voir la clé dans l'infobulle"
+L['Display offline below online'] = 'Afficher les hors ligne en-dessous des en ligne'
+L['Announce new keys to party'] = 'Annoncer les clés au groupe'
+L['Announce new keys to guild'] = 'Annoncer les clés à la guilde'
+L['EXPANDED_TOOLTIP'] = "Afficher la description des affixes dans l'infobulle"
+
+-- !keys chat command
+L['!keys chat command'] = 'Commande du chat !keys'
+L['!KEYS_DESC'] = 'Répondre à la commande !keys dans les canaux de discussion suivants'
+
+-- SYNC OPTIONS
+L['SYNC OPTIONS'] = 'OPTIONS DE SYNCRONISATION'
+L['Sync with friends'] = 'Syncroniser avec les amis'
+L['Show other faction'] = 'Afficher les autres factions'
+
+-- Rank Filter
+L['Rank Filter'] = 'Filtrer par rang'
+L['Include these ranks in the guild listing'] = 'Inclure ces rangs dans la liste des guildes'
+
+
 
 -- Shadowlands Dungeons
 
@@ -65,3 +88,20 @@ L["Operation: Mechagon - Workshop"] = 'Opération: Mécagone - Atelier'
 L["Operation: Mechagon - Junkyard"] = 'Opération: Mécagone - Casse'
 L["Tazavesh: Streets of Wonder"] = 'Tazavesh: Rue des Merveilles'
 L["Tazavesh: So'leah's Gambit"] = "Tazavesh: Stratagème de So'leah"
+
+-- Dragonflight Dungeons
+
+L["Ruby Life Pools"] = "Bassins de l'Essence Rubis"
+L["Nokhud Offensive"] = "Offensive Nokhud"
+L["Azure Vault"] = 'Caveau d"Azur'
+L["Algeth'ar Academy"] = "Académie d'Algeth'ar"
+L["Court of Stars"] = 'Cour des Étoiles'
+L["Halls of Valor"] = 'Les Salles des Valereux'
+L["Shadowmoon"] = "Terre Sacrée d'Ombrelune"
+L["Temple"] = 'Temple du Serpent de jade'
+
+L["The Nokhud Offensive"] = "L'offensive Nokhud"
+L["The Azure Vault"] = "Le Caveau d'Azur"
+L["Shadowmoon Burial Grounds"] = "Terre Sacrée d'Ombrelune"
+L["Temple of the Jade Serpent"] = 'Temple du Serpent de jade'
+
