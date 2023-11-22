839a70dc6739a8449beabfedc67e246be0ee9856
zs00556266 <zs00556266@qq.com>
2023-11-22 11:15:52 +0800

    Update zhCN.lua

--- Localization/zhCN.lua
+++ Localization/zhCN.lua
@@ -34,8 +34,8 @@ L['REQUEST_INVITE'] = REQUEST_INVITE
 L['CANCEL'] = CANCEL
 
 -- Announce messages
-L['ANNOUNCE_NEW_KEY'] = 'Astral Keys: New key %s'
-L['NO_KEY'] = 'No key'
+L['ANNOUNCE_NEW_KEY'] = 'Astral Keys: 新的钥石 %s'
+L['NO_KEY'] = '没有钥石'
 L['KEYS_RESPOND_ON_NO_KEY'] = 'Respond even if no key'
 L['KEYS_RESPOND_WITH_ALL_CHARACTERS'] = 'Respond with all characters'
 
@@ -46,3 +46,14 @@ L['FILTER_TEXT_CHARACTER'] = 'Filter by character name'
 -- Options
 L['!KEYS_DESC'] = 'Respond to !keys command in the following chat channels'
 L['EXPANDED_TOOLTIP'] = 'Show affix description in the tooltip'
+
+L["Dawn of the Infinite: Galakrond's Fall"] = '永恒黎明: 迦拉克隆的殒落'
+L["DotI: Galakrond's Fall"] = '永恒黎明: 殒落'
+L["Dawn of the Infinite: Murozond's Rise"] = '永恒黎明: 姆诺兹多的崛起'
+L["DotI: Murozond's Rise"] = '永恒黎明: 崛起'
+L["Atal'Dazar"] = '阿塔达萨'
+L["Waycrest Manor"] = '维克雷斯庄园'
+L["Darkheart Thicket"] = '黑心林地'
+L["Black Rook Hold"] = '黑鸦堡垒'
+L["The Everbloom"] = '永茂林地'
+L["Throne of the Tides"] = '潮汐王座'
