fc53085e7bfc4ba7b014b045718a5df24ddbf13f
Alex Safatli <safatli@cs.dal.ca>
2026-04-02 10:01:59 -0300

    Up version and improve early return of keys commands

--- AstralKeys.lua
+++ AstralKeys.lua
@@ -1,6 +1,6 @@
 local _, addon = ...
 addon.Week = 0
-addon.EXPANSION_LEVEL = GetMaxLevelForLatestExpansion()
+addon.EXPANSION_LEVEL = GetMaxLevelForLatestExpansion() or 90
 addon.NUM_AFFIXES = 5
 
 if not AstralKeys then AstralKeys = {} end
