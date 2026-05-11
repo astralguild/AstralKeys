38282e9cc357739a69e494ebd62dad986b4edc0b
Alex Pierce <48069006+RedAntisocial@users.noreply.github.com>
2026-05-10 20:25:14 -0400

    Tooltip taint trapping
    Updated and trapped out (via early exit) tooltip taint on Forces % against secret values.

--- Forces.lua
+++ Forces.lua
@@ -1,11 +1,17 @@
 local _, addon = ...
 
 function addon.DisplayForcesCountInTooltip(tooltip, data)
-  if not tooltip or tooltip ~= GameTooltip or not data or not data.guid then return end
+  if not tooltip or tooltip ~= GameTooltip or not data then return end
   if not addon.inKey then return end
   if not MDT or not AstralKeysSettings.general.show_tooltip_forces.isEnabled then return end
 
-  local npcID = select(6, strsplit("-", data.guid))
+  local unit = GameTooltip:GetUnit()
+  if not unit then return end
+
+  local guid = UnitGUID(unit)
+  if not guid then return end
+
+  local npcID = select(6, strsplit("-", guid))
   local count, max = MDT:GetEnemyForces(tonumber(npcID))
 
   if count and max and count ~= 0 and max ~= 0 then
